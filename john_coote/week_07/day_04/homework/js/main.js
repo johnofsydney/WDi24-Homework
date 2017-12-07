662f822b297497a4813a21b7de9bce5418105dc5
John Coote <john_coote@rocketmail.com>
2017-12-08 10:17:22 +1100

    homework finished

--- john_coote/week_07/day_04/homework/js/main.js
+++ john_coote/week_07/day_04/homework/js/main.js
@@ -132,3 +132,69 @@ var startingData = [
   [["harpo",   "marx", 1988], ["firstName", "lastName", "born"]],
   [["gummo",   "marx", 1992], ["firstName", "lastName", "born"]]
 ];
+
+
+// First step is restructuring each one of the nested arrays (_.unzip will help). For example:
+// // This...
+// [["groucho", "marx", 1990], ["firstName", "lastName", "born"]]
+//
+// // Needs to become this...
+// [["groucho", "firstName"], ["marx", "lastName"], [1990, "born"]]
+let unzippedData = []
+for (var i = 0; i < startingData.length; i++) {
+  unzippedData.push(_.unzip(startingData[i]))
+}
+console.log(unzippedData);
+
+// You then need to turn them into an object (_.object will help). For example:
+// // This...
+// [["groucho", "firstName"], ["marx", "lastName"], [1990, "born"]]
+//
+// // Needs to become this...
+// { 1990: "born", groucho: "firstName", marx: "lastName" }
+let objectifiedArray = []
+for (var i = 0; i < unzippedData.length; i++) {
+  objectifiedArray.push(_.object(unzippedData[i]))
+}
+console.log(objectifiedArray);
+//
+// You then need to flip that object around (_.invert will help). For example:
+// // This...
+// { 1990: "born", groucho: "firstName", marx: "lastName" }
+//
+// // Needs to become this...
+// { born: "1990", firstName: "groucho", lastName: "marx" }
+let invertedArray = []
+for (var i = 0; i < objectifiedArray.length; i++) {
+  invertedArray.push(_.invert(objectifiedArray[i]))
+}
+console.log(invertedArray);
+
+//
+// You then need to change each value in that object. If the value can be turned into a number, you need to minus 100 from it. If it can't be, you need to make the first letter a capital (_.mapObject, _.isNaN will help) For example:
+// // This...
+// { born: "1990", firstName: "groucho", lastName: "marx" }
+//
+// // Needs to become this...
+// { born: 1890, firstName: "Groucho", lastName: "Marx" }
+
+_.mapObject(invertedArray[0], function (val, key) { return val + " bob" } )
+
+let manipArray = []
+for (var i = 0; i < invertedArray.length; i++) {
+  manipArray.push(_.mapObject(invertedArray[i], function (val, key) {
+    if (_.isNaN(parseInt(val))) {
+      return val.charAt(0).toUpperCase() + val.slice(1);
+    } else {
+      return ( parseInt(val) - 100 )
+    }
+     } ))
+}
+
+console.log(manipArray);
+
+
+let templateVariable = _.template("<%= firstName %> <%= lastName %> was born in <%= born %>")
+for (var i = 0; i < manipArray.length; i++) {
+  console.log(templateVariable(manipArray[i]));
+}
