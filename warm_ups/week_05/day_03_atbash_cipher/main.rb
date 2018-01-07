d85e90c2d6a3b625c3c4c49815b9837d217cca4d
Joshua McEvoy <joshuamcevoy5@gmail.com>
2018-01-07 20:40:18 +1100

    warmups

--- warm_ups/week_05/day_03_atbash_cipher/main.rb
+++ warm_ups/week_05/day_03_atbash_cipher/main.rb
@@ -13,26 +13,7 @@ class Atbash
   end
 
   # The encode method will both encode and decode.
-  def encode
-
-    # create a variable to hold the encoded string
-    output = ""
-    # creating an array of the alphabet to find the indexs of the letters.
-    alphabet = ( "a".."z" ).to_a
-
-    # changing the input string to downcase then splitting it into individual characters in an array then looping through the array to gain access to each letter.
-    @input_string.downcase.split('').each do | letter |
-
-      # finding the index of the letter within the alphabet array
-      index = alphabet.index( letter )
-
-      # using that index to find the corresponding letter in the reversed array.
-      output << alphabet.reverse[ index ]
-
-    end
-    # return the output.
-    p output
-  end
+  
 
 end
 
