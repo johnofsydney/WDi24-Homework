41b1332cb072bba3c81b3011f57f6dd74d30a62f
John Coote <john_coote@rocketmail.com>
2018-07-03 23:57:02 +1000

    mta_rb

--- john_coote/week_04/day_02/mta/mta.rb
+++ john_coote/week_04/day_02/mta/mta.rb
@@ -2,9 +2,9 @@ require 'pry'
 # Set arrays of the different lines
 
 network = {
-  :N => ["Times", "34th", "28th", "23rd", "Union Square", "8th"],
-  :L => ["8th", "6th", "Union Square", "3rd", "1st"],
-  :SIX => ["Grand Central", "33rd", "28th", "23rd", "Union Square", "Astor Place"]
+  N => ["Times", "34th", "28th", "23rd", "Union Square", "8th"],
+  L => ["8th", "6th", "Union Square", "3rd", "1st"],
+  SIX => ["Grand Central", "33rd", "28th", "23rd", "Union Square", "Astor Place"]
 }
 
 # N = ["Times", "34th", "28th", "23rd", "Union Square", "8th"]
