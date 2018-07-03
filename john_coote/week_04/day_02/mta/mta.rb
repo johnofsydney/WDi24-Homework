497c8dd55e404e4ca888f7ecac90ad95dedc295b
John Coote <john_coote@rocketmail.com>
2018-07-03 23:39:30 +1000

    mta_rb

--- john_coote/week_04/day_02/mta/mta.rb
+++ john_coote/week_04/day_02/mta/mta.rb
@@ -1,23 +1,29 @@
 require 'pry'
 # Set arrays of the different lines
-N = ["Times", "34th", "28th", "23rd", "Union Square", "8th"]
-L = ["8th", "6th", "Union Square", "3rd", "1st"]
-SIX = ["Grand Central", "33rd", "28th", "23rd", "Union Square", "Astor Place"]
 
+network = {
+  :N => ["Times", "34th", "28th", "23rd", "Union Square", "8th"],
+  :L => ["8th", "6th", "Union Square", "3rd", "1st"],
+  :SIX => ["Grand Central", "33rd", "28th", "23rd", "Union Square", "Astor Place"]
+}
+
+# N = ["Times", "34th", "28th", "23rd", "Union Square", "8th"]
+# L = ["8th", "6th", "Union Square", "3rd", "1st"]
+# SIX = ["Grand Central", "33rd", "28th", "23rd", "Union Square", "Astor Place"]
 
 
 def single_trip (line, start_station, finish_station)
 
+  first_position = line.index start_station
+  last_position = line.index finish_station
 
-  first_position = line.find_index(start_station)
-  last_position = line.find_index(finish_station)
-  if (last_position < first_position)
+  if last_position < first_position
     line.reverse!
-    first_position = line.find_index(start_station)
-    last_position = line.find_index(finish_station)
+    first_position = line.index start_station
+    last_position = line.index finish_station
   end
   hop = line[first_position .. last_position]
-  puts hop
+  # puts hop
   return hop
 
 end
@@ -25,8 +31,9 @@ end
 
 def double_trip (start_line, start_station, finish_line, finish_station)
 
-    single_trip(start_line, start_station, "Union Square")
-    single_trip(finish_line, "Union Square", finish_station)
+    hop1 = single_trip(start_line, start_station, "Union Square")
+    hop2 = single_trip(finish_line, "Union Square", finish_station)
+    hop1 + hop2
 
 end
 
@@ -35,8 +42,10 @@ end
 
 
 
-single_trip(N, "Union Square", "Times")
+# p single_trip(N, "Times", "Union Square")
+# p single_trip(L, "Union Square", "8th")
 
-double_trip(N, "Times", L, "1st")
+p double_trip(network[:N], "Times", network[:SIX], "Grand Central")
+p double_trip(network[:N], network[:N][0], network[:SIX], network[:SIX].last)
 
-binding.pry
+# binding.pry
